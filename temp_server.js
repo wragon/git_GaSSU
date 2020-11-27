1564af37a2a7ade6c63be2f00e89968ce2b8d463
pencil-HB <tarabin5459@gmail.com>
2020-11-27 23:42:48 +0900

    write & DB 연결

--- temp_server.js
+++ temp_server.js
@@ -3,6 +3,14 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var fs = require('fs');
 var http = require('http');
+var mysql = require('mysql');
+var conn = mysql.createConnection({
+    host: 'localhost',
+    user : 'root',
+    password : '123456',
+    database : 'gassu'
+});
+
 
 // 웹 서버를 생성합니다.
 var app = express();
@@ -33,6 +41,26 @@ app.all('/route', function (request, response) {
 app.all('/mypage', function (request, response) {
     response.writeHead(200, {"Content-Type":"text/HTML; charset=utf-8"});
     fs.createReadStream("./public/myPage.html", {encoding: null}).pipe(response);
+});
+
+app.post('/write.html', function (request, response) {
+    var writer = 00000000;
+    var title = request.body.post_name;
+    var post_desc = request.body.post_desc;
+    var image = request.body.post_image;
+
+    var param = [writer, title, image, post_desc];
+    var sql = 'INSERT INTO sns (writer, title, sphoto, sdesc) VALUES(?,?,?,?)';
+
+    conn.query(sql, param, function(err, rows, fields){
+        if (err) {throw err;
+            console.log(err);}
+        else{
+            console.log(rows.insertId); //중요함!!!!!!!!!!!!!!!!!!!!
+            response.writeHead(200, {"Content-Type":"text/HTML; charset=utf-8"});
+            fs.createReadStream("./public/index.html", {encoding: null}).pipe(response);
+        }
+    });
 });
 
 // 웹 서버를 실행합니다.
