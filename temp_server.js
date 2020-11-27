ab51f9dd8a73e931ce72f682f7b24026876b3364
pencil-HB <tarabin5459@gmail.com>
2020-11-27 20:43:59 +0900

    기본루트 생성, html 이름 수정

--- temp_server.js
+++ temp_server.js
@@ -1,13 +1,41 @@
 ﻿// 모듈을 추출합니다.
 var express = require('express');
 var bodyParser = require('body-parser');
+var fs = require('fs');
+var http = require('http');
 
 // 웹 서버를 생성합니다.
 var app = express();
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
+//주소입력시 각각 연결
+app.all('/mainhome', function (request, response) {
+    response.writeHead(200, {"Content-Type":"text/HTML; charset=utf-8"});
+    fs.createReadStream("./public/index.html", {encoding: null}).pipe(response);
+});
+
+app.all('/place', function (request, response) {
+    response.writeHead(200, {"Content-Type":"text/HTML; charset=utf-8"});
+    fs.createReadStream("./public/place.html", {encoding: null}).pipe(response);
+});
+
+app.all('/social', function (request, response) {
+    response.writeHead(200, {"Content-Type":"text/HTML; charset=utf-8"});
+    fs.createReadStream("./public/social.html", {encoding: null}).pipe(response);
+});
+
+app.all('/route', function (request, response) {
+    response.writeHead(200, {"Content-Type":"text/HTML; charset=utf-8"});
+    fs.createReadStream("./public/route.html", {encoding: null}).pipe(response);
+});
+
+app.all('/mypage', function (request, response) {
+    response.writeHead(200, {"Content-Type":"text/HTML; charset=utf-8"});
+    fs.createReadStream("./public/myPage.html", {encoding: null}).pipe(response);
+});
+
 // 웹 서버를 실행합니다.
-app.listen(52273, function () {
-    console.log('Server Running at http://127.0.0.1:52273');
+app.listen(54321, function () {
+    console.log('Server Running at http://127.0.0.1:54321');
 });
